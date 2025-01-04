eb9a5e7013ef6e48ac01c769669f8efa9e626b10
tot121212 <totmusicbusiness@gmail.com>
2025-01-04 14:00:11 -0600

    added displayBooks, addBookToLibrary

--- bookconstructor.js
+++ bookconstructor.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, isRead){
     this.title = title,
     this.author = author,
@@ -8,5 +10,16 @@ function Book(title, author, pages, isRead){
     }
 }
 
-let book = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-console.log(book.bookInfo());
+function addBookToLibrary(book){
+    myLibrary.push(book);
+}
+
+function displayBooks(){
+    myLibrary.forEach(book => {
+        console.log(book.bookInfo());
+    });
+}
+
+addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", 295, false));
+addBookToLibrary(new Book("The Lord of the Rings", "J.R.R. Tolkien", 1178, true));
+displayBooks()
