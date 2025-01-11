1e9ca71ef448d4d400276ca3f7a7472eabcf029c
tot121212 <totmusicbusiness@gmail.com>
2025-01-10 19:58:35 -0600

    refactored to class syntax

--- bookconstructor.js
+++ bookconstructor.js
@@ -1,21 +1,22 @@
 const myLibrary = [];
-addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", 295, false));
-addBookToLibrary(new Book("The Lord of the Rings", "J.R.R. Tolkien", 1178, true));
-
-function Book(title, author, pages, isRead){
-    this.title = title,
-    this.id = this.title.replace(/\s+/g, "");
-    this.author = author,
-    this.pages = pages,
-    this.isRead = isRead,
-    this.bookInfo = function(){
-        return this.title + " by " + this.author + ", " + this.pages + " pages, " + (this.isRead ? "read" : "not read yet");
-    }
-}
-
 function addBookToLibrary(book){
     myLibrary.push(book);
 }
+
+class Book{
+    constructor(title, author, pages, isRead){
+        this.title = title;
+        this.id = this.title.replace(/\s+/g, "");
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
+    static bookInfo = function(){
+        return this.title + " by " + this.author + ", " + this.pages + " pages, " + (this.isRead ? "read" : "not read yet");
+    }
+}
+addBookToLibrary(new Book("The Hobbit", "J.R.R. Tolkien", 295, false));
+addBookToLibrary(new Book("The Lord of the Rings", "J.R.R. Tolkien", 1178, true));
 
 let tBody = null;
 function displayBooks(){
